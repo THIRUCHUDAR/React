6b7326ce52a69c9554f8420009a90621e4735f72
unknown <thiruchudars.23eie@kongu.edu>
2025-06-23 20:52:20 +0530

    day7

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return(
     <>
         <h1>About Us</h1>
-        <h1>Hi, I am Thiruchudar form dubai</h1>
+        <h1>Hi, I am Thiruchudar from Namakkal</h1>
 
     </>
     );
